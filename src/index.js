ec86df6c491f7e5e4c6164436cc1b990288d8415
Brian <137355367+brnhynh@users.noreply.github.com>
2023-11-08 14:33:42 -0800

    feat: improve wildcard feature for img output

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { imgCommand } from './commands';
 const program = new Command();
 
 program
-  .version('0.5.1')
+  .version('0.6.0')
   .description('Generate placeholders.')
   .addCommand(imgCommand)
   .parse();
